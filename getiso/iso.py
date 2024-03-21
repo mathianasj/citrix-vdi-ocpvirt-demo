44bc02aff45a357892c0355c51546dbe7dedf93e
Joshua Mathianas <mathianasj@gmail.com>
2024-03-21 10:09:57 -0400

    add some extra options to chrome

--- getiso/iso.py
+++ getiso/iso.py
@@ -6,7 +6,9 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.select import Select
 
 options = Options()
-options.headless = True
+options.add_argument('--no-sandbox')
+options.add_argument('--headless')
+options.add_argument('--disable-dev-shm-usage')
 
 driver = webdriver.Chrome(options=options)
 driver.implicitly_wait(10)
